b9146017c7f71f0a85389b8fe0bfc6008d563d14
Joan Gabrielle Basa <82226134+jgabrielle7@users.noreply.github.com>
2024-11-01 14:47:07 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-
+// jQuery to show a message when Enroll button is clicked
+$(document).ready(function() {
+    $('.enroll-btn').click(function() {
+        alert("Thank you for enrolling! You'll receive a confirmation email soon.");
+    });
+});
